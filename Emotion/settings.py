f9891e8fd68cff058a3106fccf5b3f3c936b034e
Amartya Basu <51461919+amrtya@users.noreply.github.com>
2021-07-02 23:56:27 +0530

    Update settings.py
    Handle Security Issues

--- Emotion/settings.py
+++ Emotion/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4$n0@ka)=3(c&bm$15%0_#8@x6%3#+czx2u6a2p(re3up&0@0g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
